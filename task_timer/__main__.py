7db7972ffea641442a0e48a4292d59399cdd22b4
David Marin <142943922+cunecoder@users.noreply.github.com>
2025-02-10 23:28:11 -0600

    Update __main__.py

--- task_timer/__main__.py
+++ task_timer/__main__.py
@@ -4,33 +4,78 @@
 # Last Updated: 1/29/2025
   
 import click
+import json
+from task_class import Task
+
+tasks = []
+
+jsontasksfile = "tasksfile.json"
+
+@click.group()
+def main():
+    """ Create a click group for all other functions. """
+    pass
+
+def save_task():
+    """ Save each tasks to json file. """
+    with open(jsontasksfile, 'w') as jsonfile:
+        for task in tasks:
+            json.dump(task.create_dictionary(), jsonfile, indent=6)
+
+
+@main.command()
+@click.argument("name")
+def create_task(name):
+    """ Create a new task using click. """
+
+    # Making sure no duped tasks
+    for task in tasks:
+        if task.name == task_name:
+            console.print("Task name taken.")
+            return
+    task = Task(name)
+    tasks.append(task)
+
+    # Immediately save task to a file
+    save_task()
+
+
+
+@main.command()
+def access_task():
+    """ Load tasks from json file into tasks list. """
+    with open(jsontasksfile, 'r') as jsonfile:
+        tasks_dictionarys = json.load(jsonfile)
+        return [Task.convert_dict(task_dictionary) for task_dictionary in tasks_dictionarys]
+    
+    return []
+
+@main.command()
+@click.argument('taskname')
+def end_task(taskname):
+    """ End a task and display total time of said task. """
+    for task in tasks:
+        if task.name == taskname:
+            task.end_task()
+            
+
+
+
+@main.command()
+def answer():
+    print("The answer is 100% because I know I can figure this out.")
+
 
 
 # @click.command()
-# @click.option('--name')
-# def create_task(name):
-#     """ Create a new task using click. """
-#     print("TEST WORKED")
-#     task = Task("First Task")
-#     task.display_name()
-
-
-def question():
-    print("What are the chances of this working?")
-
-def answer():
-    print("The answer is 100/%/ because I know I can figure this out.")
-
-@click.command()
-@click.argument('choice')
-@click.option('-createtask', '-ct', help='Create task with inputted name')
-
-def main(choice, createtask):
-    """ This is my main cli. """
-    #if choice = 'printq':
-        #question()
+# @click.argument('choice')
+# @click.option('-createtask', '-ct', help='Create task with inputted name')
+# def main(choice, createtask):
+#     """ This is my main cli. """
+#     #if choice = 'printq':
+#         #question()
     
-    click.echo(f"Testing first; name of your task is {createtask}, but arg is {choice}!")
+#     click.echo(f"Testing first; name of your task is {createtask}, but arg is {choice}!")
 
 if __name__ == '__main__':
     main()
